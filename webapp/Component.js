df8acee8dc4d041aba356f6b84dd2813547a5291
Gonzalo Meana Balseiro <55688528+GonzaloMB@users.noreply.github.com>
2021-09-30 16:25:35 +0200

    Add files via upload

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/ui/generic/app/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("com.au.gonzalomb.z_demo_flight_cds.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
